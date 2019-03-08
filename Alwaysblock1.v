ee47ee12313d89446ef4f5ce2a6551df36108b79
M <mmmmmmjjjh@hotmail.com>
2019-03-08 08:56:28 +0800

    Create Alwaysblock1.v
    **A note on wire vs. reg: The left-hand-side of an assign statement must be a net type (e.g., wire), while the left-hand-side of a procedural assignment (in an always block) must be a variable type (e.g., reg). These types (wire vs. reg) have nothing to do with what hardware is synthesized, and is just syntax left over from Verilog's use as a hardware simulation language.**

---
+++ Alwaysblock1.v
@@ -0,0 +1,18 @@
+// synthesis verilog_input_version verilog_2001
+
+
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    
+   assign out_assign = a & b;
+    always @ (*)
+        begin
+            out_alwaysblock = a & b;
+        end
+
+endmodule
+
