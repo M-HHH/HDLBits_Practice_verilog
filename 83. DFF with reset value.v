4fe7a9cdbd274e266c90540f994407da377707c2
M <mmmmmmjjjh@hotmail.com>
2019-03-21 15:35:21 +0800

    Create 83. DFF with reset value.v

---
+++ 83. DFF with reset value.v
@@ -0,0 +1,20 @@
+module top_module (
+    input clk,
+    input reset,
+    input [7:0] d,
+    output [7:0] q
+);
+    
+    always @ (negedge clk)
+    begin
+    	if(reset)
+    		begin
+    				q <= 8'h0x34;
+    		end
+    	else
+        begin
+            q <= d;
+        end
+    end
+
+endmodule
